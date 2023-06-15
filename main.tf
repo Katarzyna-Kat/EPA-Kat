75da5828893b698f33e42bd75bf31c1b558ff575
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2023-06-15 14:12:26 +0100

    apply works

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ data "archive_file" "zip" {
   source_file = "snapshot-deletion.py"
   output_path = "snapshot-deletion.zip"
 }
+
 data "aws_iam_policy_document" "policy" {
   statement {
     sid    = ""
@@ -18,10 +19,12 @@ data "aws_iam_policy_document" "policy" {
     actions = ["sts:AssumeRole"]
   }
 }
+
 resource "aws_iam_role" "snapshot_deletion_lambda" {
   name               = "snapshot_deletion_lambda"
   assume_role_policy = data.aws_iam_policy_document.policy.json
 }
+
 resource "aws_lambda_function" "lambda" {
   function_name = "snapshot-deletion"
   filename         = data.archive_file.zip.output_path
