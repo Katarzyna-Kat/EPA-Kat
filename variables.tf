32cfd992c714a92f3051f92acfb0db427198c609
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2023-06-20 15:04:25 +0100

    fixed lambda

--- variables.tf
+++ variables.tf
@@ -5,24 +5,3 @@ variable "aws_region" {
 variable "email" {
   default     = "k.polonkiewicz@gmail.com"
 }
-
-variable "sns_subscription_email_address_list" {
-   type = list(string)
-   description = "List of email addresses"
- }
- 
- variable "sns_subscription_protocol" {
-   type = string
-   default = "email"
-   description = "SNS subscription protocal"
- }
- 
- variable "sns_topic_name" {
-   type = string
-   description = "SNS topic name"
- }
- 
- variable "sns_topic_display_name" {
-   type = string
-   description = "SNS topic display name"
- }
