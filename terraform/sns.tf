cc6c6391afd3786350e008944083dcde3b2bbcc1
Katarzyna-Kat <77454630+Katarzyna-Kat@users.noreply.github.com>
2023-06-23 14:10:31 +0100

    Update sns.tf

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -7,12 +7,3 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   protocol  = "email"
   endpoint  = var.email
 }
-
-data "template_file" "access_sns_snapshot_deletion" {
-  template = file("functions_folder/main_code/snapshot_deletion.py")
-
-  vars = {
-    snapshot_deletion_sns_topic = aws_sns_topic.user_updates.arn
-  }
-}
-
