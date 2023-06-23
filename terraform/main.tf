de7c6d8283b10d97f260afebccbd508e32d0251b
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2023-06-23 13:29:03 +0100

    testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,28 +3,11 @@ terraform {
     bucket                  = "tf-state-epa"
     key                     = "terraform-epa"
     region                  = "eu-north-1"
-    shared_credentials_file = "~/.aws/credentials"
   }
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::tf-state-epa"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-      "Resource": "arn:aws:s3:::tf-state-epa/terraform-epa"
-    }
-  ]
 }
 
 
